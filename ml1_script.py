96a3102a29c57656c730b276cf5c611a5504e9d3
RaNxxx <ranlanli.0821@gmail.com>
2018-02-18 21:12:28 +0900

    昔作った習ったテキスト分類器

--- ml1_script.py
+++ ml1_script.py
@@ -11,7 +11,20 @@ def open_corpus(corpus_path):
     corpus_path(str) に指定されたファイルを開き、改行を消してひとつの文字列(str)にして返す
     """
 
-    pass
+    file_open = open(corpus_path,"r")
+
+    file_lines = file_open.readlines()
+    file_list = [element.replace("\n","") for element in file_lines]
+
+    file_str = ""
+
+    for element in file_list:
+        file_str += element
+
+    return file_str
+
+#corpus_path = "corpus/dazai_melos.txt"
+#print(open_corpus(corpus_path))
 
 
 def tokenize(text):
@@ -25,29 +38,16 @@ def tokenize(text):
     # janomeの使い方は、http://mocobeta.github.io/janome/ 参照。
     # http://mocobeta.github.io/janome/api/janome.html#janome.tokenizer.Token も要チェック。
 
-    pass
+    t = Tokenizer()
+    l = []
 
+    for token in t.tokenize(text):
+        l.append(token.surface)
 
-def per_word_cross_entropy(model, text):
-    """
-    model(NgramModel)とtext(str)をとり、modelにおけるtextのper-word クロスエントロピー(float)を返す
-    """
+    return l
 
-    # ヒント
-    # >>> model.entropy(words)
-    # で words(list of str) で構成されるテキストの文全体のエントロピーが出せるので、これを文の単語数で割れば良い
-
-    pass
-
-
-def get_average_entropy(model, corpus_path, max_lines_to_load=-1):
-    """
-    corpus_path(str)に指定したファイルの冒頭 max_lines_to_load(int) 行の文の言語モデルオブジェクト model(NgramModel)
-    で計ったクロスエントロピーの平均値を返す。
-    例）max_lines_to_load = 30 の場合、冒頭30行の平均 per-word クロスエントロピーを返す
-    """
-
-    pass
+#text = open_corpus(corpus_path)
+#print(tokenize(text))
 
 
 def train_model(corpus_path, n=2):
@@ -63,11 +63,65 @@ def train_model(corpus_path, n=2):
     # words(list of str) は訓練データとなる文を、単語ごとに切ったもの。
     # estimatorは、Smoothing(平滑化)といって、1度も出ていない単語の出現確率を他の単語から推定する手法のための推定器で、ここではおまじないと考えて良い
 
+    text = open_corpus(corpus_path)
+    model = NgramModel(n, tokenize(text), estimator)
+
     return model
 
+#n = 3
+#print(train_model(corpus_path, n))
+
+
+def per_word_cross_entropy(model, text):
+    """
+    model(NgramModel)とtext(str)をとり、modelにおけるtextのper-word クロスエントロピー(float)を返す
+    """
+
+    # ヒント
+    # >>> model.entropy(words)
+    # で words(list of str) で構成されるテキストの文全体のエントロピーが出せるので、これを文の単語数で割れば良い
+
+    n = len(tokenize(text))
+    m = model.entropy(tokenize(text))
+
+    return m/n
+
+#model = train_model(corpus_path, n)
+#print(per_word_cross_entropy(model, "走れ、ひろきメロ！お前はやればできる子だ。信じとる！"))
+#print(per_word_cross_entropy(model, "ひろきは、下賤の者で単純な男であった"))
+#print(per_word_cross_entropy(model, "ランは天才である"))
+
+
+def get_average_entropy(model, corpus_path, max_lines_to_load=-1):
+    """
+    corpus_path(str)に指定したファイルの冒頭 max_lines_to_load(int) 行の文の言語モデルオブジェクト model(NgramModel)
+    で計ったクロスエントロピーの平均値を返す。
+    例）max_lines_to_load = 30 の場合、冒頭30行の平均 per-word クロスエントロピーを返す
+    """
+
+    file_open = open(corpus_path,"r")
+
+    file_lines = file_open.readlines()
+    file_list = [element.replace("\n","") for element in file_lines]
+
+    lines = file_list[:max_lines_to_load]
+    total_entropy = 0
+
+    for element in lines:
+        if not len(element) == 0:
+            total_entropy += per_word_cross_entropy(model,element)
+
+    average_entropy = total_entropy / len(lines)
+    return average_entropy
+
+#max_lines_to_load = 30
+#print(get_average_entropy(model, corpus_path, max_lines_to_load))
+
+
+
 if __name__ == "__main__":
-    n = 4
-    max_lines_to_load = 60
+    n = 5
+    max_lines_to_load = 100
 
     # 宮沢賢治 銀河鉄道の夜を学習して言語モデルをつくる
     model = train_model("./corpus/kenji_ginga.txt", n)
